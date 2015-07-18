78bb1f8dc650b57477589e7174ed977ca6956c51
Raymond Wong <raymond.1993.wong@gmail.com>
2015-07-18 09:12:19 +0800

    finish score function

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,7 +1,6 @@
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
-#include "MainScene.h"
-#include "Config.h"
+#include "Demo.h"
 
 USING_NS_CC;
 
@@ -13,13 +12,23 @@ AppDelegate::~AppDelegate()
 {
 }
 
+//if you want a different context,just modify the value of glContextAttrs
+//it will takes effect on all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    //set OpenGL context attributions,now can only set six attributions:
+    //red,green,blue,alpha,depth,stencil
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
 bool AppDelegate::applicationDidFinishLaunching() {
     // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = GLView::create("Orbitum");
-        glview->setFrameSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        glview = GLViewImpl::create("My Game");
         director->setOpenGLView(glview);
     }
 
@@ -30,7 +39,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    auto scene = MainScene::createScene();
+    auto scene = HelloWorld::createScene();
 
     // run
     director->runWithScene(scene);
